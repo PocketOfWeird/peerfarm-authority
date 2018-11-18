097e7301060509119aeb6bc4a69b055069fb3a6d
Hartzler <ftn0813@missouristate.edu>
2018-11-18 16:17:32 -0600

    Fixed knowhosts reducer bug

--- data/reducer/known_hosts.js
+++ data/reducer/known_hosts.js
@@ -1,14 +1,12 @@
-const { SET_KNOWN_HOSTS, SET_NODE_INFO } = require('../actions');
+const { SET_KNOWN_HOSTS } = require('../actions');
 
 
 const known_hosts = (state=[], action) => {
   switch (action.type) {
     case SET_KNOWN_HOSTS:
-        return action.payload;
-    case SET_NODE_INFO + '_FROM_PEER':
-        let newState = state.filter(host => host.ip !== action.payload.ip);
-        newState.push(action.payload);
-        return newState;
+      let newState = state.filter(host => host.ip !== action.payload.ip);
+      newState.push(action.payload);
+      return newState;
     default:
       return state;
   }
